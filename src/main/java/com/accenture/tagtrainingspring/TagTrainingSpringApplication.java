bdc4f56e247ebf12a9093b10a60380c77bf1f9cc
HughButler <hugh.butler@accenture.com>
2023-10-31 13:20:55 +0000

    END OF DAY 1

--- src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
+++ src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
@@ -3,8 +3,13 @@ package com.accenture.tagtrainingspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.accenture.tagtrainingspring.patient.Patient;
+import com.accenture.tagtrainingspring.screening.Screening;
+
 @SpringBootApplication
 public class TagTrainingSpringApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(TagTrainingSpringApplication.class, args);
@@ -21,6 +26,12 @@ public class TagTrainingSpringApplication {
 				"    |_/_/    \\_\\_____|    |_|_|  \\__,_|_|_| |_|_|_| |_|\\__, |\n" +
 				"                                                        __/ |\n" +
 				"                                                       |___/ \n");
-	}
+		Patient patient = new Patient();
+		Screening screening = new Screening();
+		patient.setName("Hugh Butler");
+		screening.setMalignant(false);
 
+		System.out.println(patient.getName()+" has a cancer status of "+screening.isMalignant());
+
+	}
 }
